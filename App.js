75dc99c19f8d40ceef329457b091b1f78da9930b
Delta Student <deepaks9631342845@gmail.com>
2025-03-31 11:12:32 +0530

    revising basic of html, javascript and css

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
+/*
+
+<div id="parent">
+<div id="child">
+  <h1>hello world from react</h1>
+</div>
+</div>
+*/
+
+const parent=React.createElement("div", {id:parent},  React.createElement("div", {id: "child"}, React.createElement("h1", {}, "hello world from react")));
+
+const parent1=React.createElement("div", {id:parent}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "hello world from react")));
+
+
 const heading=React.createElement(
   "h1",
-  {},
+  {id: "heading"},
   "hello world from react"
 );
+console.log(parent1); // return object
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent1); 
